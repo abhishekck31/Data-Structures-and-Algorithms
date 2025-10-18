7e50b4a0796bbff25b12ca40f403bf352e53cfa6
abhishekck31 <abhishekkannur31@gmail.com>
2025-10-18 17:27:11 +0530

    Bubble Sort

---
+++ Data Structures and Algorithms/3.SortingAlgorithms/BubbleSort.java
@@ -0,0 +1,22 @@
+public class BubbleSort {
+    public static int[] sort(int[] arr){
+        int n = arr.length;
+        for(int i = 0 ; i < n - 1 ; i++){
+            for(int j = 0 ; j < n - i - 1 ; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 5, 6};
+        int[] sortedArr = sort(arr);
+        for(int i : sortedArr) {
+            System.out.print(i + " ");
+        }
+    }
+}
