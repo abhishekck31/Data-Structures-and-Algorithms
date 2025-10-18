18175f3abb94c8a30f53410046a4f26746721984
abhishekck31 <abhishekkannur31@gmail.com>
2025-10-18 09:17:42 +0530

    Factorial of a number

--- Data Structures and Algorithms/2.BasicRecrsion/Basic Problems/factorial.java
+++ Data Structures and Algorithms/2.BasicRecrsion/Basic Problems/factorial.java
@@ -1,14 +1,14 @@
 public class factorial {
     public static void main(String[] args){
         int n = 5;
-        int fact = factorial(n);
+        int fact = factorialRecursive(n);
         System.out.println("Factorial of " + n + " is: " + fact);
     }
-    public static int factorial(int n){
+    public static int factorialRecursive(int n){
         if(n==0){
             return 1;
         }else{
-            return n * factorial(n-1);
+            return n * factorialRecursive(n-1);
         }
     }
 }
